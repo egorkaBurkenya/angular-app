5c9d77ddf643f3c4fdc60d7e20342b0ec1802676
egorkaBurkenya <sleepyegorka@gmail.com>
2022-12-08 18:22:15 +0300

    init

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpErrorResponse, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, delay, Observable, throwError } from "rxjs";
+import { IProduct } from "../models/product";
+import { ErrorService } from "./error.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+    constructor(
+        private http: HttpClient,
+        private errorService: ErrorService) {
+    }
+
+    getAll(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
+            params: new HttpParams({
+                fromObject: {limit: 5}
+            })
+        }).pipe(
+            delay(2000),
+            catchError(this.errorHandler.bind(this))
+        )
+    }
+
+    private errorHandler(err: HttpErrorResponse) {
+        this.errorService.handle(err.message)
+        return throwError(() => err.message)
+    }
+}
